72d34d8dc2659a67b3375f155c64b56b27277697
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-11 16:26:29 +0800

    update

--- teamup-frontend/pages/HomePage.tsx
+++ teamup-frontend/pages/HomePage.tsx
@@ -1,5 +1,57 @@
-function HomePage() {
-  return <h1>Home Page</h1>
+// function HomePage() {
+//   return <h1>Home Page</h1>
+// }
+
+// export default HomePage
+
+import { Text, Container, Anchor, MediaQuery, Button } from '@mantine/core'
+import { MdOutlineArrowDownward } from 'react-icons/md'
+const HomePage = () => {
+  //const theme = useMantineTheme();
+
+  return (
+    <section id='about'>
+      <Container fluid>
+        <div className='about-content'>
+          <div style={{ marginBottom: 15 }}>
+            <Text transform='uppercase' weight={500} color='yellow'>
+              MADE WITH REACT AND MANTINE
+            </Text>
+          </div>
+
+          <div style={{ marginBottom: 15 }}>
+            <Text>
+              <MediaQuery
+                query='(max-width: 768px)'
+                styles={{ fontSize: '2.8rem !important' }}
+              >
+                <h1 className='title'>Simple Mantine Template</h1>
+              </MediaQuery>
+            </Text>
+          </div>
+
+          <div style={{ marginBottom: 25 }}>
+            <Text size='xl' color='black'>
+              Simple showcase of this powerful and well implemented library
+              called <Anchor href='https://mantine.dev/'>Mantine</Anchor>.
+            </Text>
+          </div>
+
+          <div className='buttons'>
+            {/* <Link to='section-one' smooth duration={500}>
+              <Button color='yellow' radius='lg' size='md'>
+                Tell me more
+              </Button>
+            </Link> */}
+
+            <Button variant='default' radius='lg' size='md'>
+              Other stuff
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
 }
 
 export default HomePage
