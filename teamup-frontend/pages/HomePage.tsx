24ea21dbce01e3d2d32e23b50269f519552aa72b
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-11 16:53:39 +0800

    HomePage

--- teamup-frontend/pages/HomePage.tsx
+++ teamup-frontend/pages/HomePage.tsx
@@ -3,52 +3,77 @@
 // }
 
 // export default HomePage
+import { Carousel } from '@mantine/carousel'
+import { Text, Container, useMantineTheme, Title } from '@mantine/core'
+import Header from './components/Header'
 
-import { Text, Container, Anchor, MediaQuery, Button } from '@mantine/core'
-import { MdOutlineArrowDownward } from 'react-icons/md'
+// import { Text, Container, Anchor, MediaQuery, Button } from '@mantine/core'
+// import { MdOutlineArrowDownward } from 'react-icons/md'
 const HomePage = () => {
   //const theme = useMantineTheme();
 
+  const theme = useMantineTheme()
+
+  const carouselContent = {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column' as 'column',
+    backgroundColor: theme.colors.teal[5],
+    borderRadius: 15,
+    gap: 15,
+  }
+
   return (
     <section id='about'>
-      <Container fluid>
-        <div className='about-content'>
-          <div style={{ marginBottom: 15 }}>
-            <Text transform='uppercase' weight={500} color='yellow'>
-              MADE WITH REACT AND MANTINE
-            </Text>
-          </div>
+      <Header></Header>
+      <Container>
+        <Text color='black' align='center' mb='15px'>
+          <Title order={1}>Biggest Events</Title>
+        </Text>
 
-          <div style={{ marginBottom: 15 }}>
-            <Text>
-              <MediaQuery
-                query='(max-width: 768px)'
-                styles={{ fontSize: '2.8rem !important' }}
-              >
-                <h1 className='title'>Simple Mantine Template</h1>
-              </MediaQuery>
-            </Text>
-          </div>
+        <Text color='black' align='center' mb='25px'>
+          Some of the best events happening this month
+        </Text>
 
-          <div style={{ marginBottom: 25 }}>
-            <Text size='xl' color='black'>
-              Simple showcase of this powerful and well implemented library
-              called <Anchor href='https://mantine.dev/'>Mantine</Anchor>.
-            </Text>
-          </div>
-
-          <div className='buttons'>
-            {/* <Link to='section-one' smooth duration={500}>
-              <Button color='yellow' radius='lg' size='md'>
-                Tell me more
-              </Button>
-            </Link> */}
-
-            <Button variant='default' radius='lg' size='md'>
-              Other stuff
-            </Button>
-          </div>
-        </div>
+        <Carousel
+          withIndicators
+          height={200}
+          slideSize='33.333333%'
+          slideGap='md'
+          breakpoints={[
+            { maxWidth: 'md', slideSize: '50%' },
+            { maxWidth: 'sm', slideSize: '100%', slideGap: 15 },
+          ]}
+          loop
+          align='start'
+          pr='10px'
+          pl='10px'
+        >
+          <Carousel.Slide>
+            <div style={carouselContent}>
+              <Title order={2}>1. Join sports events</Title>
+              <Text>
+                Join the excitement where you want! Meet fellow sports
+                enthusiasts and enjoy being active. Make new connections and
+                have a great time. Find now!
+              </Text>
+            </div>
+          </Carousel.Slide>
+          <Carousel.Slide>
+            <div style={carouselContent}>
+              <Title order={2}>2</Title>
+              <Text>New an Event.</Text>
+            </div>
+          </Carousel.Slide>
+          <Carousel.Slide>
+            <div style={carouselContent}>
+              <Title order={2}>3</Title>
+              <Text>My Events</Text>
+            </div>
+          </Carousel.Slide>
+        </Carousel>
       </Container>
     </section>
   )
